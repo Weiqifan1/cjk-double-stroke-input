de027c5f8cc300a9947aac0a8982ddf4578b09b6
Lykke <cmlykke@hotmail.com>
2024-01-02 00:43:51 +0100

    can read junda files

---
+++ double-stroke-input/projectFolder/CreateDeckFileController.cs
@@ -0,0 +1,14 @@
+﻿using double_stroke_input.projectFolder.StaticFileMaps;
+
+namespace double_stroke_input.projectFolder;
+
+public class CreateDeckFileController
+{
+    public void myFunction()
+    {
+        Console.WriteLine("Hello, this is my function.");
+        GenerateJundaMap genJunda = new GenerateJundaMap();
+        genJunda.Run();
+    }
+    
+}
